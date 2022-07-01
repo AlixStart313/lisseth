d7c548a7d10efe4c94cb6452ec4e42f436cf7cca
Alexander <20213tn014@utez.edu.mx>
2022-07-01 14:59:17 -0500

    practica 2 y 3 de apps web  unidad 2

---
+++ js/jsT2.js
@@ -0,0 +1,26 @@
+function color1(){ 
+    document.getElementById('uno').style.background ="green"
+}
+
+function color2(){ 
+document.getElementById('dos').style.background = "red"
+}
+
+function color3(){ 
+document.getElementById('tres').style.background ="green"
+}
+
+function color4(){ 
+document.getElementById('cuatro').style.background = "red"
+}
+
+function color5(){ 
+document.getElementById('cinco').style.background ="green"
+}
+
+function color6(){ 
+document.getElementById('seis').style.background = "red"
+}
+
+
+
---
+++ js/jsT3.js
@@ -0,0 +1,12 @@
+function imagenAnterior(){
+    var galeria=['<img src="imagenes/flor1.png" id="imagen1"></img>',
+                 '<img src="imagenes/flor2.png" id="imagen1></img>',
+                 '<img src="imagenes/flor3.png" id="imagen1"></img>',
+                 '<img src="imagenes/flor4.png" id="imagen1></img>',
+                 '<img src="imagenes/flor5.png" id="imagen1"></img>',
+                ];
+    var i=1;
+           switch(i){
+            1:
+           } 
+    }
